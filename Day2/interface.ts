b99bc4c007e2e7c5921124b704027584e1e599be
Svngoku <marymarty104@gmail.com>
2019-03-16 16:08:24 +0100

    readme interface

--- Day2/interface.ts
+++ Day2/interface.ts
@@ -6,7 +6,6 @@ interface Book {
 }
 
 /* function to show book */
-
 let showBook = (book: Book) =>
 	console.log(`
 		Titre: ${book.title} 
@@ -15,11 +14,10 @@ let showBook = (book: Book) =>
 	`);
 
 /*	Create a new Book */  
-
 let JungleBook = {
 	title: "Le livre de la Jungle" , 
-	resume: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec ipsum quis ex cursus interdum a a augue. Sed dolor magna, tristique sed turpis quis, feugiat commodo ipsum. Nulla elementum leo nec libero varius aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur suscipit tincidunt neque nec bibendum. Phasellus ante purus, accumsan ac vestibulum eu, eleifend vel arcu. Nam sed vehicula neque. Maecenas rhoncus nisi vitae enim aliquet varius. " , 
-	page : 21
+	resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec ipsum quis ex cursus interdum a a augue. Sed dolor magna, tristique sed turpis quis, feugiat commodo ipsum. Nulla elementum leo nec libero varius aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur suscipit tincidunt neque nec bibendum. Phasellus ante purus, accumsan ac vestibulum eu, eleifend vel arcu. Nam sed vehicula neque. Maecenas rhoncus nisi vitae enim aliquet varius. " , 
+	page: 21
 }
 
 showBook(JungleBook)
