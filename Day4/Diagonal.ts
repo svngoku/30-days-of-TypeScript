90de6dc5383691efd289f557a215c6f81099b78a
Svngoku <marymarty104@gmail.com>
2019-03-22 21:12:22 +0100

    Day4 | Update diagonal file

--- Day4/Diagonal.ts
+++ Day4/Diagonal.ts
@@ -1,13 +1,11 @@
 function DiagonalDifference(arr:Array<Array<number>>): number {
-    let len:number = arr.length;
+    let len:number = arr.length, diag1 = 0, diag2 = 0;
     let diag:number = arr.length - 1;
-    console.log(arr);
     for(let i = 0 ; i < len; i++) {
-     var diag1: number = arr[i][i];
-     var diag2: number = arr[i][diag];
-     console.log(diag1, diag2);
+     diag1 += arr[i][i];
+     diag2 += arr[i][diag];
      diag--;
     }
-    return Math.abs(diag1 - diag2);
-  }
-  console.log(DiagonalDifference([[11, 2, 4], [4,5,6], [10,8,-12]]))
+    return Math.abs(diag2 - diag1);
+}
+console.log(DiagonalDifference([[11, 2, 4], [4,5,6], [10,8,-12]]))
