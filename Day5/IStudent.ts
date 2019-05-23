e07ddc8acfbf6fff66570f3e1111f677adca762f
Svngoku <marymarty104@gmail.com>
2019-05-23 03:59:11 +0200

    Day5 { Interface }

---
+++ Day5/IStudent.ts
@@ -0,0 +1,8 @@
+export default interface IStudent {
+    name: string
+    lastname: string
+    age: number
+    address: string
+    phone: number
+    // notes?: [number]
+}
---
+++ Day5/Student.ts
@@ -0,0 +1,18 @@
+import IStudent from './IStudent'
+
+let students = [];
+
+function createStudent(infos: IStudent): void {
+    let { name , lastname , address , age, phone } = infos;
+    let newStudent = infos;
+    students.push(newStudent);
+}
+
+function getStudents(student): any  {
+    return students;
+}
+
+let Student1 = createStudent({ name: 'STARK' , lastname: 'Tonny', address: '3 RUE NELSON' ,age: 45, phone: 8789065 });
+
+console.log(getStudents(students));
+
