952c3eef6a00511c0ec2ae55d89dd5fa85551156
Svngoku <marymarty104@gmail.com>
2019-06-13 16:51:47 +0200

    Day6 : Genric class Updated 🦖

--- Day6/Generic.ts
+++ Day6/Generic.ts
@@ -1,15 +1,19 @@
 // @ts-check
 
+// KeyVlaue Pair class
 class KeyValuePair<T,U>
 {
+    // properties
     private key: T;
     private value: U;
 
+    // constructor of class
     constructor(key: T, value: U) {
         this.key = key;
         this.value = value;
     }
 
+    // display method : log the key and the value instanciate
     display(): void {
         console.log(`Key: ${this.key}, value: ${this.value}`);
     }
--- Day6/IGeneric.ts
+++ Day6/IGeneric.ts
@@ -1,9 +1,10 @@
+// interface IKeyValueProcessor
 interface IKeyValueProcessor<T,U> {
     process(key: T, value: U): void;
 };
 
 
-
+// Class Key Value Process implements from IKeyValueProcessor
 class kvProcessor<T,U> implements IKeyValueProcessor<T,U> {
     process(key: T, value: U): Object {
         return {
@@ -13,6 +14,7 @@ class kvProcessor<T,U> implements IKeyValueProcessor<T,U> {
     }
 }
 
+// exports Inteface with Class
 
 export {
     IKeyValueProcessor,
