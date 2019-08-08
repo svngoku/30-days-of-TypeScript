690ebf87475fcbc94be1fe6829674c6cdbc303b4
Svngoku <marymarty104@gmail.com>
2019-08-08 21:11:42 +0200

    enums

--- Day8/main.ts
+++ Day8/main.ts
@@ -2,8 +2,22 @@ import { Sport } from './Sport';
 import { Transport } from './Transport';
 import { Courses } from './Courses'
 
-
+// destructures object 
+// Courses fields
 let { chimie , maths , programmation } = Courses;
 
+// Transport fields
+let { train, bike, car , bus  } = Transport;
 
-console.log()
+// Sport fields
+
+let { basketball , soccer, football } = Sport;
+
+
+
+
+console.log(JSON.stringify({
+    Courses ,
+    Transport,
+    Sport
+}))
